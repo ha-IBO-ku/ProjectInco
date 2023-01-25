36390c7e2d358c861dfa305fefb7534edb230e59
ha-IBO-ku <steubenstr24eg@gmail.com>
2023-01-25 15:25:28 +0900

    2023_1_25_ver1
    エラー吐きます

--- Assets/PlayerShipPrefab.cs
+++ Assets/PlayerShipPrefab.cs
@@ -10,9 +10,49 @@ public class PlayerShipPrefab : MonoBehaviour
     GameObject body;
     Rigidbody bodyRB;
     GameObject[] thrusters = new GameObject[8];
-    Rigidbody[] thrusterRBs = new Rigidbody[8];
+    Rigidbody[] thrustRBs = new Rigidbody[8];
     FixedJoint[] thrustFJs = new FixedJoint[8];
     Vector3 thrusterScale = new Vector3(0.1f, 0.1f, 0.3f);
+    Vector3[] thDirVec = new Vector3[8]
+        {
+        (0f,1f,1f),
+        (1f,0f,1f),
+        (0f,-1f,1f),
+        (-1f,0f,1f),
+        (0f,1f,-1f),
+        (1f,0f,-1f),
+        (0f,-1f,-1f),
+        (-1f,0f,-1f)
+        };
+    ThrustRotation[] thRot = new ThrustRotation[8]
+        {
+        (45f,0f,0f),
+        (0f,45f,0f),
+        (-45f,0f,0f),
+        (0f,-45f,0f),
+        (-45f,0f,0f),
+        (0f,-45f,0f),
+        (45f,0f,0f),
+        (0f,45f,0f)
+        };
+
+    
+
+
+
+    struct ThrustRotation
+    {
+        public float X;
+        public float Y;
+        public float Z;
+
+        public ThrustRotation(float x,float y,float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
 
 
 
@@ -28,28 +68,26 @@ public class PlayerShipPrefab : MonoBehaviour
         body.transform.localScale = new Vector3(1, 1, 1);
         bodyRB = body.AddComponent<Rigidbody>();
 
-        //GameObject[] thrusters = new GameObject[8];
-        //Rigidbody[] thrusterRBs = new Rigidbody[8];
-        //FixedJoint[] thrustFJs = new FixedJoint[8];
-        //Vector3 thrusterScale = new Vector3(0.1f, 0.1f, 0.3f);
+
 
 
         //本番コード
-        //for (int i = 0; i < thrusters.Length; i++)
-        //{
-        //    thrusters[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        //    thrusterRBs[i] = thrusters[i].AddComponent<Rigidbody>();
-        //    thrusters[i].name = "thruster" + i.ToString();
-        //    thrusters[i].transform.localScale = thrusterScale;
-        //}
+        for (int i = 0; i < thrusters.Length; i++)
+        {
+            thrusters[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            thrustRBs[i] = thrusters[i].AddComponent<Rigidbody>();
+            thrusters[i].name = "thruster" + i.ToString();
+            thrusters[i].transform.localScale = thrusterScale;
+            thrustFJs[i] = ThrusterLocate(thrusters[i], thDirVec[i], thRot[i]);
+        }
 
         //テストコード↓
         
         
-        thrusters[0] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        thrusterRBs[0] = thrusters[0].AddComponent<Rigidbody>();
-        thrusters[0].name = "thruster0";
-        thrusters[0].transform.localScale = thrusterScale;
+        //thrusters[0] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //thrustRBs[0] = thrusters[0].AddComponent<Rigidbody>();
+        //thrusters[0].name = "thruster0";
+        //thrusters[0].transform.localScale = thrusterScale;
         //ここまで
 
 
@@ -60,20 +98,25 @@ public class PlayerShipPrefab : MonoBehaviour
         //thrusters[0].transform.position = vec3;
         //thrusters[0].transform.rotation = Quaternion.Euler(45,0, 0);
         //thrustFJs[0] = thrusters[0].AddComponent<FixedJoint>();
-        //thrustFJs[0].connectedBody = bodyRigidBody;
+        //thrustFJs[0].connectedBody = bodyRB;
 
         //Debug.Log(vec3.ToString());
 
-        thrustFJs[0] = ThrusterLocate(thrusters[0], new Vector3(0, 1, 1), 45, 0, 0);
-
+        //thrustFJs[0] = ThrusterLocate(thrusters[0], new Vector3(0, 1, 1), 45, 0, 0);
+        //thrustFJs[1] = ThrusterLocate(thrusters[1], new Vector3(1, 0, 1), 0, 45, 0);
+        //for (int i = 0; i < thrustFJs.Length; i++)
+        //{
+        //    thrustFJs[i] = ThrusterLocate(thrusters[i], thDirVec[i], thRot[i]);
+        //}
 
     }
-    private FixedJoint ThrusterLocate(GameObject go, Vector3 vec3, float a, float b, float c)
+    
+    private FixedJoint ThrusterLocate(GameObject go, Vector3 vec3, ThrustRotation tR)
     {
         vec3.Normalize();
         vec3 = vec3 * ((1 + thrusterScale.y) / 2);
         go.transform.position = vec3;
-        go.transform.rotation = Quaternion.Euler(a, b, c);
+        go.transform.rotation = Quaternion.Euler(tR.X, tR.Y, tR.Z);
         FixedJoint rtnObj = go.AddComponent<FixedJoint>();
         rtnObj.connectedBody = bodyRB;
         return rtnObj;
